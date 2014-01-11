49a2ef990e8a0d1dcd3a2e9db565d3f5819eb75f
HumbertoFadel <fadelbh@hotmail.com>
2014-01-11 03:31:20 -0200

    Alterando a ideia de usar dicionario e trocando por tupla. Clean no codigo.

--- alert.py
+++ alert.py
@@ -6,35 +6,17 @@ __author__ = 'Humberto'
 from operator import itemgetter
 import os, re
 
-#Funcao de validacao dos dados
-def verificar(x):
-    while x == '':
-       	print 'Esse campo e obrigatorio!'
-       	x = input('Digite novamente: \n')
-    return x
+#Tupla de IP's cadastrados
+ips2 = [("FNT-ESCCA-AR-A01", "173.194.42.1"), ("FNT-ESCCA-AR-A02", "173.194.42.2"), ("FNT-ESCCA-AR-A03", "173.194.42.3"),
+       ("FNT-ESCCA-AR-A04",	"173.194.42.4"), ("FNT-ESCCA-AR-A05", "173.194.42.5"), ("FNT-ESCCA-AR-A06",	"173.194.42.6"),
+       ("FNT-ESCCA-AR-A07",	"173.194.42.7"), ("FNT-ESCCA-AR-A08", "173.194.42.8"), ("FNT-ESCCA-AR-A09",	"173.194.42.9"),
+       ("FNT-ESCCA-AR-A10",	"173.194.42.10"), ("FNT-ESCCA-AR-A11",	"173.194.42.11"), ("FNT-ESCCA-AR-A12",	"173.194.42.12"),
+       ("FNT-ESCCA-AR-A13",	"173.194.42.13"), ("FNT-ESCCA-AR-A14",	"173.194.42.14"), ("FNT-ESCCA-AR-A15",	"173.194.42.15"),
+       ("FNT-ESCCA-AR-A16",	"173.194.42.16"), ("FNT-ESCCA-AR-A17",	"173.194.42.17"), ("FNT-ESCCA-AR-A18",	"173.194.42.18"),
+       ("FNT-ESCCA-AR-A19",	"173.194.42.19"), ("FNT-ESCCA-AR-A20",	"173.194.42.20"), ("FNT-ESCCA-AR-A21",	"173.194.42.21"),
+       ("FNT-ESCCA-AR-A22",	"173.194.42.22"), ("FNT-ESCCA-AR-A23",	"173.194.42.23"), ("FNT-ESCCA-IN-I01",	"173.194.42.24")]
+ips2.sort() #Ordenando a Tupla
 
-#Cadastro de novos Elementos e IP's
-#----------------------------------------------------------------------------------
-ips = {}
-v = "s"
-while v == "s":
-    print "Cadastro de Novos Elementos!"
-    print
-    v = raw_input("Voce deseja cadastrar novos elementos (S/N)? ")
-    print
-    if v == "s" or v == "S":
-        element = raw_input("Informe o Elemento: ")
-        i = raw_input("Informe o IP: ")
-        ips[i] = element
-    elif v == "n" or v == "N":
-        print "Cadastro OK!"
-        break
-    else:
-        print "Opcao invalida!"
-
-#Organizando o dicionario de acordo com os values
-#----------------------------------------------------------------------------------
-ips2 = sorted(ips.items(), key=itemgetter(1))
 #Bloco informativo de quais IP's serao verificados
 #----------------------------------------------------------------------------------
 print
@@ -43,24 +25,25 @@ print "Ip's a serem verificados."
 print "+" * 62
 print
 for ip2 in ips2:
-    print "Elemento: %18s - IP: %s" %(ip2[1], ip2[0])
+    print "Elemento: %s" %ip2[0]
 print
 
 #Bloco que executa a verificacao de resposta dos IP's
 #----------------------------------------------------------------------------------
-print "Verificando conexao dos IP's..."
-print
+print "=" * 62
+print "              Verificando resposta dos IP's..."
+print "=" * 62
 print "-" * 62
 print "     Elemento      |     Ip Fonte     |        Status        |"
 print "-" * 62
-for ip in ips.keys():
-    cmd = "ping -c3 " + ip
+for ip in ips2:
+    cmd = "ping -c2 " + ip[1]
     r = "".join(os.popen(cmd).readlines()) #Executa o comando de ping e captura a linha de resposta
     if re.search ("64 bytes from", r): #Faz analise da linha capturada anteriormente e informa o status da conexao
         print "-" * 62
-        print "%18s | %16s |    Conexao OK        |" %(ips[ip], ip)
+        print "%18s | %14s   |      Conexao OK      |" %(ip[0], ip[1])
     else:
         print "-" * 62
-        print "%18s | %16s |    Falha na Conexao  |" %(ips[ip], ip)
+        print "%18s | %14s   |    Falha na Conexao  |" %(ip[0], ip[1])
 print "-" * 62
 print
